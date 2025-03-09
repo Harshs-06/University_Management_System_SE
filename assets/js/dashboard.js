7b81e0687494550c1b61ca9fae3ad4a1ff29a944
Harshs-06 <harsh.soni.23cse@bmu.edu.in>
2025-03-09 14:15:13 +0530

    secondCommit

---
+++ assets/js/dashboard.js
@@ -0,0 +1,27 @@
+
+let searchBar=document.querySelector("#searchBar");
+searchBar.placeholder="Search"
+
+
+
+let profile_userName = document.querySelector("#Profile_userName");
+profile_userName.textContent="Harsh Soni";
+
+let profile_userDetail = document.querySelector("#Profile_userDetail");
+profile_userDetail.textContent="2nd year, B.tech(CSE VI)";
+
+let profile_pic = document.querySelector("#ProfilePic");
+profile_pic.src="/assets/images/userProfilePic.png"
+profile_pic.width="35";
+profile_pic.height="35";
+
+let settings_icon = document.querySelector("#settings_icon");
+settings_icon.src="/assets/images/settings_icon.png"
+settings_icon.width="24"
+settings_icon.height="24"
+
+
+let notification_icon = document.querySelector("#notification_icon");
+notification_icon.src="/assets/images/notification_icon.png";
+notification_icon.width="24"
+notification_icon.height="24"
