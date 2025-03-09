5d078b4b8015666084e5b4813d93cec196f8f2b2
Harshs-06 <harsh.soni.23cse@bmu.edu.in>
2025-03-09 18:54:13 +0530

    third commit

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -25,3 +25,56 @@ let notification_icon = document.querySelector("#notification_icon");
 notification_icon.src="/assets/images/notification_icon.png";
 notification_icon.width="24"
 notification_icon.height="24"
+
+
+// let todo_items = document.querySelector("#todo_items");
+// let items = [["study","Preparation for ML mid sem","start at 20:00"]];
+// let item_category_icon= {"study":"/assets/images/item_study.png","warn":"/assets/images/item_warning.png"};
+// function your_todo_items() {
+//     for (let i = 0; i < items.length; i++) {
+//         let div = document.createElement('div');
+//         div.className="items_div"
+        
+        
+//         let icon = document.createElement('img');
+//         icon.src = item_category_icon[items[i][0]];
+//         icon.className="item_cat_icon";
+//         icon.style.width = "24px";
+        
+      
+        
+//         let title = document.createElement('p');
+//         title.textContent = items[i][1];
+//         title.className="item_title"
+       
+        
+//         let time = document.createElement('p');
+//         time.className="item_time"
+//         time.textContent = items[i][2];
+       
+       
+        
+        
+       
+//         div.appendChild(icon);
+//         div.appendChild(title);
+//         div.appendChild(time);
+        
+//         todo_items.appendChild(div);
+//     }
+// }
+
+// your_todo_items()
+    
+document.querySelectorAll('.sub_option').forEach(option => {
+    option.addEventListener('click', function() {
+        document.querySelectorAll('.sub_option').forEach(opt => opt.classList.remove('selected'));
+        this.classList.add('selected');
+    });
+});
+
+
+
+
+
+
